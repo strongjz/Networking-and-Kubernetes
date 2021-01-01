3dfd3d8f1cf948906ce1a076dc2cce0db283d6ad
James Strong <strong.james.e@gmail.com>
2021-01-01 15:21:05 -0600

    moving things around

---
+++ chapter-2/vagrant-host-2/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["vagrant-reload",["= 0.0.1"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.24.0"]]],"checksum":"2f104ac7ec2bae1c1d3e03385e9d1e62199a99c8874dc91ed92a3b44c2a200e6","vagrant_version":"2.2.10"}
